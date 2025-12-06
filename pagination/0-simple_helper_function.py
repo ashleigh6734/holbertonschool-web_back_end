1e04312a6b5d435b66df741b3f8e8f87e012d8a9
ashleigh6734 <ashleigh6734@gmail.com>
2025-12-06 13:54:08 +1100

    pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,7 +4,7 @@ Task 0: Simple helper function
 """
 
 
-def index_range(page:int, page_size:int) -> tuple:
+def index_range(page: int, page_size: int) -> tuple:
     """Return a tuple of size two containing a start index and an end index
     corresponding to the range of indexes to return in a list for
     those particular pagination parameters.
