f57e18ae9c94f9d4f3192fd9964ab97de492b558
ashleigh6734 <ashleigh6734@gmail.com>
2025-12-23 15:44:03 +1100

    task3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,44 @@
+export default class Currency {
+  constructor(code, name) {
+    // Type validation
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    // Underscore attributes
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getters
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  // Setters
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  // Method
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
